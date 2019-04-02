3788e1731682d5f414a391b0574900ba39eb0d2a
Tiago Lubiana <tiagolubiana@hotmail.com>
2019-04-02 15:29:24 -0300

    Adding simulation for estimating p-value

---
+++ R/kxk_test.R
@@ -0,0 +1,18 @@
+#' kxk_test
+#'
+#' Uses a 100 000 simulation of randomly sampled genes from a 13000 gene pool to
+#' obtain estimates of links by random selection of genes.
+#' This estimates are valid only for 1000 receptors and 1000 ligands.
+#'
+#' @param n The number of links found
+#' @return Prints and estimate of the probability o
+#' @export
+#' @example
+#'bridge_kxk_test(15)
+
+kxk_test <- function(n){
+  number_of_links <- kxk_simulation
+  p_value <- sum(number_of_links >= n)/length(number_of_links)
+  print( paste0('The estimated p-value (probability of occurrence of a equal or more extreme value under the null) for ', n, ' links is of ', p_value, '.') )
+}
+
